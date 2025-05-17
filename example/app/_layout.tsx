2a46f481f6578da28aeb08727dd086bc2f107ccf
Henry Paulino <me@henrypl.com>
2025-05-16 23:44:08 -0400

    update

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -18,7 +18,6 @@ mite.init()
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   })
@@ -35,13 +34,11 @@ export default function RootLayout() {
 
   return (
     <MiteProvider miteInstance={mite}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
     </MiteProvider>
   )
 }
