39e41fdef6ebd76bc63ac0b5ad5119333686d558
Henry Paulino <me@henrypl.com>
2026-04-13 23:43:07 +0100

    Move docs route to root path
    - Move [[...slug]] catch-all route from /docs to /
    - Merge DocsLayout into root layout, remove separate docs layout
    - Remove landing page in favor of docs as homepage
    - Update source baseUrl from /docs to /

--- docs/src/lib/source.ts
+++ docs/src/lib/source.ts
@@ -3,5 +3,5 @@ import { loader } from 'fumadocs-core/source'
 import { docs } from '../../.source/server'
 
 export const source = loader(docs.toFumadocsSource(), {
-  baseUrl: '/docs',
+  baseUrl: '/',
 })
